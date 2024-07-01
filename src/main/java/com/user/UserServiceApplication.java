a910bbbffe616368eeec594185a59856073f6eb4
hrideshkumar <ch.hridesh@gmail.com>
2024-07-01 22:22:00 +0530

    circuit breaker patter added

--- src/main/java/com/user/UserServiceApplication.java
+++ src/main/java/com/user/UserServiceApplication.java
@@ -1,21 +1,14 @@
 package com.user;
 
-import org.out.OutPackageBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
-@SpringBootApplication(scanBasePackages = "org.out")
+@SpringBootApplication
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
-		ApplicationContext ctx =SpringApplication.run(UserServiceApplication.class, args);
-		//System.out.println(ctx.getClass().getName());
-//		SpringBean sb = ctx.getBean(SpringBean.class);
-//		sb.print();
+		SpringApplication.run(UserServiceApplication.class, args);
 		
-		OutPackageBean sb = ctx.getBean(OutPackageBean.class);
-		sb.print();
 	}
 
 }
